c078ae52fddbb09f3d2a6dc0427f0653794116f2
DevGrl <DevGrl@users.noreply.github.com>
2016-07-26 15:14:00 -0700

    Update SampleQueries.sql

--- SampleQueries.sql
+++ SampleQueries.sql
@@ -88,7 +88,7 @@ join (select count(*) NumberOfReports, e2.managerid
 	) y
   ON
 	y.ManagerID = x.EmployeeID
-	*/
+	
   --DO THIS. More efficient
   select ManagerID , count(employeeid) NumberOfReports
   from employee
@@ -99,3 +99,43 @@ join (select count(*) NumberOfReports, e2.managerid
   from employee
   where managerid in (select managerid from employee)
   group by managerid
+  */
+
+  /*
+  --8 Same as #7 but only for managers that have 5 or more reports
+  
+  SELECT x.DepartmentName, x.EmployeeID, x.FirstName, x.LastName, y.NumberOfReports
+  FROM 
+	(
+	  SELECT d.DepartmentName, e.EmployeeID, e.FirstName, e.LastName
+		  FROM employee e
+		  JOIN department d ON e.departmentid = d.departmentid
+		  WHERE employeeid in (SELECT managerid FROM employee)
+	) x
+  JOIN
+	(
+	  SELECT ManagerID , count(employeeid) NumberOfReports
+		FROM employee
+		GROUP BY managerid
+	) y
+  ON
+	y.ManagerID = x.EmployeeID
+WHERE y.NumberOfReports > 5	
+*/
+
+/*
+--9) Generate a list of all employees with their job titles and salaries
+Select e.FirstName, e.LastName, j.JobTitle, e.Salary
+from employee e
+join job j on j.jobid = e.jobid
+*/
+
+/*
+--11) What is the min, max, and average salary for each position ?
+select j.JobTitle, j.MinSalary, j.MaxSalary, e.AvgSalary
+ from job j
+ join 
+ (select jobid, avg(salary) AvgSalary from employee
+ group by jobid) e
+ on j.jobid = e.jobid
+ */
